8792d60487c1b7b7a8281e0b4bad39853cd11f52
Skafee <skafee.dev@gmail.com>
2022-11-17 20:47:24 +0100

    Recherche tableau dynamique

--- TP3/src/couleur_compteur.c
+++ TP3/src/couleur_compteur.c
@@ -46,14 +46,21 @@ int main() {
         
     }
 
+    // Boucle qui affiche les couleurs
+	for (int i = 0; i < 100; i++)
+	{
+	    printf("0x%hhx\t 0x%hhx\t 0x%hhx\t 0x%hhx\t %d\n",colors[i].red,colors[i].green,colors[i].blue,colors[i].alpha, colors[i].occurence);
+	}
+
+    printf("--------\n");
+
     // ********************************************************
-    // Partie non fonctionnel sur la création d'un nouveau tableau avec seulement une occurence de chaque couleur
+    // Partie non fonctionnel sur la création d'un nouveau tableau avec seulement une occurrence pour chaque couleur
 
     // redéfinition de la structure couleur
 
     // typedef struct colorDistincte
     // {
-    //     //en unsigned char on veut des valeurs de 0 à 255
     //     int red;
     //     int green;
     //     int blue;
@@ -64,41 +71,63 @@ int main() {
     // Création d'un tableau dynamique sur un maximum de 100 éléments pour stocker les couleurs distinctes.
 
     // distinctColor* colorsD = (distinctColor*) malloc(sizeof(colorsD) * 100 );
-    // int nbColorD = 0;
+    // int nbColorD = 1;
 
     // recherche des couleurs distincte et stockage
 
     // for (int i = 0; i < 100; i++)
     // {
     //     int isHere = 0;
-    //     for (int j = 0; j < colorsD; j++)
-    //     {
-    //         if (colors[i].red == colorsD[j].red && colors[i].green == colorsD[j].green && colors[i].blue == colorsD[j].blue && colors[i].alpha == colorsD[j].alpha )
-    //         {             
-    //             isHere = 1;
-    //         }
-            
-    //     }
-    //     if (isHere == 0)
-    //     {
-    //         colorsD[nbColorD-1].red = colors[i].red;
-    //         colorsD[nbColorD-1].green = colors[i].green;
-    //         colorsD[nbColorD-1].blue = colors[i].blue;
-    //         colorsD[nbColorD-1].alpha = colors[i].alpha;
-    //         colorsD[nbColorD-1].occurence = colors[i].occurence;
+    //     int j = 0;
 
-    //         nbColorD++;
-    //     }
+        // while (isHere == 0 && j < 100)
+        // {
+        //     if (colors[i].red == colorsD[j].red && colors[i].green == colorsD[j].green && colors[i].blue == colorsD[j].blue && colors[i].alpha == colorsD[j].alpha )
+        //     {             
+        //         isHere = 1;
+        //     }
+            
+        //     if (isHere == 0)
+        //     {
+        //         // *colorsD[nbColorD-1].red = &colors[i].red;
+        //         // colorsD[nbColorD-1].red = colors[i].red;
+        //         // colorsD[nbColorD-1].green = colors[i].green;
+        //         // colorsD[nbColorD-1].blue = colors[i].blue;
+        //         // colorsD[nbColorD-1].alpha = colors[i].alpha;
+        //         // colorsD[nbColorD-1].occurence = colors[i].occurence;
+
+        //         nbColorD++;
+        //         // printf("%d\n", nbColorD);
+        //     }
+        //         printf("%d\n", isHere);
+
+        //     j++;
+        // }
+
+
+        // TEST 
+            // *colorsD[i].red = &colors[i].red;
+            // colorsD[nbColorD-1].red = colors[i].red;
+            // colorsD[nbColorD-1].green = colors[i].green;
+            // colorsD[nbColorD-1].blue = colors[i].blue;
+            // colorsD[nbColorD-1].alpha = colors[i].alpha;
+            // colorsD[nbColorD-1].occurence = colors[i].occurence;
+
+        
+            // distinctColor *newColor;
+            // newColor->red = colors[i].red;
+
+            // *(colorsD+i*sizeof(distinctColor)) = &newColor;
     // }
 
     // free(colorsD);
     // ********************************************************    
     
-    // Boucle qui affiche les couleurs
-	for (int i = 0; i < 100; i++)
-	{
-	    printf("0x%hhx\t 0x%hhx\t 0x%hhx\t 0x%hhx\t %d\n",colors[i].red,colors[i].green,colors[i].blue,colors[i].alpha, colors[i].occurence);
-	}
+    
+	// for (int i = 0; i < 100; i++)
+	// {
+	//     printf("0x%hhx\t 0x%hhx\t 0x%hhx\t 0x%hhx\t %d\n",colorsD[i].red,colorsD[i].green,colorsD[i].blue,colorsD[i].alpha, colorsD[i].occurence);
+	// }
 
 	return 0;
 
